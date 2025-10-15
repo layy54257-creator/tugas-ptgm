1bc149704796587b8758120fdb274e34406d03d8
layy54257-creator <layy54257@gmail.com>
2025-10-15 14:40:04 +0700

    class.py

---
+++ class.py
@@ -0,0 +1,30 @@
+class Sekolah:
+   pass
+     
+class Siswa:
+   pass
+     
+sekolah1 = Sekolah()
+sekolah1.nama = "SMK PGRI 35 Solokan Jeruk"
+sekolah1.alamat = "JL RHO Kosasih No 90 Cibodas"
+
+sekolah2 = Sekolah()
+sekolah2.nama = "SMKN 9 Bandung"
+sekolah2.alamat = "JL Soekarno-Hatta No 10"
+
+print(sekolah1.nama)
+print(sekolah1.alamat)
+print(sekolah2.nama)
+print(sekolah2.alamat)
+
+
+print(type(sekolah1))
+print(type(sekolah2))
+
+siswa1 = Siswa()
+siswa2 = Siswa()
+
+
+print(type(siswa1))
+print(type(siswa2))
+
