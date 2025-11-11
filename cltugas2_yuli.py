e39124f1c361847821c77d5ccda2e49a3e5b70e1
layy54257-creator <layy54257@gmail.com>
2025-11-11 14:05:06 +0700

    Create cltugas2_yuli.py

---
+++ cltugas2_yuli.py
@@ -0,0 +1,31 @@
+class Alatmusik :
+  nama1 = "suling" 
+  jenis1 = "suling sunda"
+  bahan1 = "kayu"
+  cara_main1 = "tiup"
+  asal1 = "Jawa Barat"
+  harga1 = "30.000"
+  nama2 = "kecapi" 
+  jenis2 = "kecapi sunda"
+  bahan2 = "bambu"
+  cara_main2 = "dipetik"
+  asal2 = "Jawa Barat"
+  harga2 = "100.000"
+  
+Alatmusik1 = Alatmusik()
+Alatmusik2 = Alatmusik()
+
+print(Alatmusik1.nama1)
+print(Alatmusik1.jenis1)
+print(Alatmusik1.bahan1)
+print(Alatmusik1.cara_main1)
+print(Alatmusik1.asal1)
+print(Alatmusik1.harga1)
+print(Alatmusik2.nama2)
+print(Alatmusik2.jenis2)
+print(Alatmusik2.bahan2)
+print(Alatmusik2.cara_main2)
+print(Alatmusik2.asal2)
+print(Alatmusik2.harga2)
+
+
